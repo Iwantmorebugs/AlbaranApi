488bf0d54efe7a27312eae631b420878b19b82c5
zakaria wakrim <zakaria.wakrim@tui.be>
2020-05-12 17:12:55 +0200

    CleanUp

--- AlbaranApi/Startup.cs
+++ AlbaranApi/Startup.cs
@@ -69,7 +69,7 @@ namespace AlbaranApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment() || env.IsEnvironment("Development")) app.UseDeveloperExceptionPage();
 
             // app.UseHttpsRedirection();
             app.UseHsts();
@@ -78,7 +78,6 @@ namespace AlbaranApi
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
